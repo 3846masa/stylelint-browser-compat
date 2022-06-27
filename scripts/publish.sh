feaa08d8f8297a663d6c9ae5ca89af7680ebe838
3846masa <3846masahiro+git@gmail.com>
2022-06-27 09:23:11 +0900

    fix(ci): fix publish command

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,8 +5,8 @@ set -eux
 unset "${!npm_config_@}"
 unset "${!NPM_CONFIG_@}"
 
-PACKAGE_NAME=$(npm -s run env echo '$npm_package_name')
-PACKAGE_VERSION=$(npm -s run env echo '$npm_package_version')
+PACKAGE_NAME=$(jq -r '.name' ./package.json)
+PACKAGE_VERSION=$(jq -r '.version' ./package.json)
 
 ## Set latest tag
 npm dist-tag add "${PACKAGE_NAME}@${PACKAGE_VERSION}" latest --userconfig /tmp/.npmrc
