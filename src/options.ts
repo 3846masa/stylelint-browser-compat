8378b9933f49bc5c5f3a99a4dd73fbbcc789d3d1
3846masa <3846masahiro+git@gmail.com>
2025-11-15 23:50:58 +0900

    fix: fallback to default options if options is not passed

--- src/options.ts
+++ src/options.ts
@@ -2,14 +2,16 @@ import { z } from 'zod';
 
 export type Options = z.infer<typeof ruleOptionsSchema>;
 
-export const ruleOptionsSchema = z.object({
-  allow: z
-    .object({
-      features: z.array(z.string()).default(() => []),
-      flagged: z.boolean().default(false),
-      partialImplementation: z.boolean().default(false),
-      prefix: z.boolean().default(true),
-    })
-    .prefault({}),
-  browserslist: z.union([z.string(), z.array(z.string())]).optional(),
-});
+export const ruleOptionsSchema = z
+  .object({
+    allow: z
+      .object({
+        features: z.array(z.string()).default(() => []),
+        flagged: z.boolean().default(false),
+        partialImplementation: z.boolean().default(false),
+        prefix: z.boolean().default(true),
+      })
+      .prefault({}),
+    browserslist: z.union([z.string(), z.array(z.string())]).optional(),
+  })
+  .prefault({});
